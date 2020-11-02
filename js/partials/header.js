7afc17ca13065066814c12ad4f191982559835c1
Oscar Dahlquist <43441099+Vattenkruka@users.noreply.github.com>
2020-11-02 16:29:05 +0100

    Vattenkruka fix: Refactoring of header and footer

---
+++ js/partials/header.js
@@ -0,0 +1,25 @@
+document.getElementById("header").innerHTML = `<header>
+<!--NAVBAR/HEADER-->
+<nav class="navbar navbar-expand lg navbar-dark bg-dark">
+    <a class="navbar-brand" href="http://127.0.0.1:5500/studious-octo-adventure/">Komputer</a>
+<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+<span class="navbar-toggler-icon"></span>
+</button>
+<div class="collapse navbar-collapse" id="navbarText">
+<ul class="navbar-nav mr-auto">
+  <li class="nav-item active">
+    <a class="nav-link" href="http://127.0.0.1:5500/studious-octo-adventure/">Home <span class="sr-only">(current)</span></a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="#">Features</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="#">Pricing</a>
+  </li>
+</ul>
+<span class="navbar-text">
+  Made by Vattenkruka
+</span>
+</div>
+  </nav>
+</header>  `
